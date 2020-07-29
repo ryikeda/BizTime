af405497261071df85f861054f4d31fb657420df
rafael ubuntu <ryikeda@outlook.com>
2020-07-29 14:25:51 +0900

    handled if not match for GET / companies route

--- routes/companies.js
+++ routes/companies.js
@@ -3,6 +3,7 @@ const ExpressError = require("../expressError");
 const router = express.Router();
 const db = require("../db");
 
+// GET Routes
 router.get("/", async (req, res, next) => {
   try {
     const results = await db.query(`SELECT * FROM companies`);
@@ -18,6 +19,8 @@ router.get("/:code", async (req, res, next) => {
     const results = await db.query(`SELECT * FROM companies WHERE code=$1`, [
       code,
     ]);
+    if (!results.rows.length)
+      throw new ExpressError(`No company found with code: ${code}`, 404);
     return res.send({ company: results.rows[0] });
   } catch (err) {
     return next(err);
