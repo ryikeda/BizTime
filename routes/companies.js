e151aef3249c53561d3d2fa9fbb3013b5fbb2edb
rafael ubuntu <ryikeda@outlook.com>
2020-07-29 14:20:03 +0900

    added route to get company by code

--- routes/companies.js
+++ routes/companies.js
@@ -7,8 +7,20 @@ router.get("/", async (req, res, next) => {
   try {
     const results = await db.query(`SELECT * FROM companies`);
     return res.json({ companies: results.rows });
-  } catch (e) {
-    return next(e);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.get("/:code", async (req, res, next) => {
+  try {
+    const { code } = req.params;
+    const results = await db.query(`SELECT * FROM companies WHERE code=$1`, [
+      code,
+    ]);
+    return res.send({ company: results.rows[0] });
+  } catch (err) {
+    return next(err);
   }
 });
 
