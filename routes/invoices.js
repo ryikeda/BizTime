fb8a362fdc96425595ba7d980fcab8143f3d1082
rafael ubuntu <ryikeda@outlook.com>
2020-07-29 20:33:02 +0900

    added tests for /invoices routes

--- routes/invoices.js
+++ routes/invoices.js
@@ -49,7 +49,7 @@ router.post("/", async (req, res, next) => {
       `INSERT INTO invoices (comp_code, amt) VALUES ($1, $2) RETURNING id, comp_code, amt, paid, add_date, paid_date`,
       [comp_code, amt]
     );
-    return res.json({ invoice: result.rows[0] });
+    return res.status(201).json({ invoice: result.rows[0] });
   } catch (err) {
     return next(err);
   }
--- routes/invoices.test.js
+++ routes/invoices.test.js
@@ -25,8 +25,36 @@ afterAll(async () => {
   await db.end();
 });
 
-describe("test test", () => {
-  test("test", async () => {
-    expect(1).toEqual(1);
+// GET Routes
+describe("GET /invoices", () => {
+  test("Get a list of one invoice", async () => {
+    const response = await request(app).get("/invoices");
+    expect(response.statusCode).toBe(200);
+    expect(response.body.invoices[0].comp_code).toEqual("inv_test_code");
+    expect(response.body.invoices[0].amt).toEqual(999);
+  });
+});
+
+describe("GET /invoices/:id", () => {
+  test("Gets a one invoice", async () => {
+    const response = await request(app).get(`/invoices`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.invoices[0].comp_code).toBe("inv_test_code");
+  });
+  test("Responds with 404 for invalid code", async () => {
+    const response = await request(app).put(`/inventory/0`);
+    expect(response.statusCode).toEqual(404);
+  });
+});
+
+// POST Route
+describe("POST /invoices", () => {
+  test("Creates a single invoice", async () => {
+    const response = await request(app)
+      .post("/invoices")
+      .send({ comp_code: "inv_test_code", amt: 111 });
+    expect(response.statusCode).toBe(201);
+    expect(response.body.invoice.comp_code).toBe("inv_test_code");
+    expect(response.body.invoice.amt).toBe(111);
   });
 });
