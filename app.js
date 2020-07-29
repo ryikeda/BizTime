77b35f538d2695556735987720717b60135fa59f
rafael ubuntu <ryikeda@outlook.com>
2020-07-29 14:04:03 +0900

    database setup and route to show all companies

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 /** BizTime express application. */
 
-
 const express = require("express");
-
 const app = express();
-const ExpressError = require("./expressError")
+const ExpressError = require("./expressError");
 
 app.use(express.json());
 
+// Routes
+const companiesRoutes = require("./routes/companies");
+
+app.use("/companies", companiesRoutes);
 
 /** 404 handler */
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   const err = new ExpressError("Not Found", 404);
   return next(err);
 });
@@ -23,9 +25,8 @@ app.use((err, req, res, next) => {
 
   return res.json({
     error: err,
-    message: err.message
+    message: err.message,
   });
 });
-
 
 module.exports = app;
--- db.js
+++ db.js
@@ -1,2 +1,20 @@
 /** Database setup for BizTime. */
 
+const { Client } = require("pg");
+
+let DB_URI;
+
+if (process.env.NODE_ENV === "test") {
+  DB_URI = "postgres://postgres:docker@localhost:5432/biztime_test";
+} else {
+  debugger;
+  DB_URI = "postgres://postgres:docker@localhost:5432/biztime";
+}
+
+let db = new Client({
+  connectionString: DB_URI,
+});
+
+db.connect();
+
+module.exports = db;
